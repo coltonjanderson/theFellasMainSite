5740270b160656e5d2903d4536223d4ac58b4082
coltonjanderson <articdrift2015@gmail.com>
2022-03-09 21:01:32 -0500

    changed to php and added gs300 img

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
         <link rel="stylesheet" href="index.css">
         <script src="index.js"></script>
     </head>
-    <body id="body" onload="init()">
 
+    <body id="body" onload="init()">
+        <button>Sign Up</button>
     </body>
 </html>
