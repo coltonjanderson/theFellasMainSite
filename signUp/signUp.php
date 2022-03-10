864f6b08e7a44a6ad252145d335c418ef6187179
coltonjanderson <articdrift2015@gmail.com>
2022-03-09 21:17:02 -0500

    attempt to fix php files

---
+++ signUp/signUp.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="index.css">
+        <script src="index.js"></script>
+    </head>
+
+    <body id="body" onload="init()">
+        <form>
+        <input type="checkbox"></input>
+        </form>
+    </body>
+</html>
